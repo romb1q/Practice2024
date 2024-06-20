68d1fd155c8ebb9195477b0fc1f67c6712478256
Roman Biruk <105756991+romb1q@users.noreply.github.com>
2024-06-20 11:26:11 +0300

    Update preCreateReference.js

--- JS Core Beginner/Variables_Var_Let_Const/preCreateReference.js
+++ JS Core Beginner/Variables_Var_Let_Const/preCreateReference.js
@@ -1,1 +1,9 @@
 // 3 task
+
+console.log(x);  // Cannot access 'x' before initialization
+console.log(y);  // undefined
+console.log(z);  // Cannot access 'z' before initialization
+
+let x = 1;
+var y = 2;
+const z = 3;
