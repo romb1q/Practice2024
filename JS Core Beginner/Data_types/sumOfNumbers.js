bf0333ef58be51fff1e8a690fac1de6d8ea1779b
Roman Biruk <105756991+romb1q@users.noreply.github.com>
2024-06-20 12:21:46 +0300

    Update sumOfNumbers.js

--- JS Core Beginner/Data_types/sumOfNumbers.js
+++ JS Core Beginner/Data_types/sumOfNumbers.js
@@ -1,1 +1,9 @@
+// 3 task
 
+function sumOfNumbers(num) {
+    let numStr = num.toString();
+    let firstNum = parseInt(numStr[0]);
+    let lastNum = parseInt(numStr[numStr.length - 1]);
+    return firstNum + lastNum
+}
+console.log(sumOfNumbers(3432124412432)); // 5
