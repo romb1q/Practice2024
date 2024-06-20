f3a564f44ad8821553b9e894b51a3c87bb7ef126
Roman Biruk <105756991+romb1q@users.noreply.github.com>
2024-06-20 11:58:53 +0300

    Update compareProperties.js

--- JS Core Beginner/Data_types/compareProperties.js
+++ JS Core Beginner/Data_types/compareProperties.js
@@ -1,1 +1,32 @@
+// 1 task
 
+const IPhone = new Object({
+    name: 'iPhone',
+    price: 1000,
+    color: 'black',
+    screenSize: 5.5
+});
+
+const Samsung = {
+    name: 'Samsung',
+    price: 1000,
+    color: 'white',
+    screenSize: 5.5
+};
+
+function compareObjects(firstObject, secondObject) {
+    const keys = Object.keys(secondObject);
+    let valuesCounter = 0;
+
+    for (let key of keys) {
+        if (firstObject.hasOwnProperty(key)) {
+            if (firstObject[key] === secondObject[key]) {
+                valuesCounter++;
+            }
+        }
+    }
+    return valuesCounter;
+}
+
+
+console.log(compareObjects(IPhone, Samsung)); // 2
