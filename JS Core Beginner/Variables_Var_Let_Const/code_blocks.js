d827a8e33e8d6fe66d9dfa300c165a5c65db725e
Roman Biruk <105756991+romb1q@users.noreply.github.com>
2024-06-20 11:13:58 +0300

    Update code_blocks.js

--- JS Core Beginner/Variables_Var_Let_Const/code_blocks.js
+++ JS Core Beginner/Variables_Var_Let_Const/code_blocks.js
@@ -1,1 +1,20 @@
+const global_z = 1;  // глобальная область видимости
 
+
+function someFunc() {  // локальная область видимости
+    let x = 2;
+    var y = 3;
+    const z = 4;
+    
+    for (let i = 0; i < 10; i++) {  // локальная область видимости
+        let local_x = 2;
+        var local_y = 3;
+    }
+    console.log(local_x);  // ReferenceError: local_x is not defined
+    console.log(local_y);  // 3
+}
+someFunc();
+
+
+console.log(global_z);  // 1
+console.log(z, x, y);   // ReferenceError: z is not defined  and  ReferenceError: x is not defined  and  ReferenceError: y is not defined
