96d0bbf2387dcd019b64ffbd10ce1d10b4dd899d
Roman Biruk <105756991+romb1q@users.noreply.github.com>
2024-06-20 10:20:46 +0300

    Update vowelsInString.js

--- JS Core Beginner/use_strict/vowelsInString.js
+++ JS Core Beginner/use_strict/vowelsInString.js
@@ -1,1 +1,12 @@
-
+'use strict';
+function findVowelsLettesInString(string) {
+    let vowels = ['a', 'e', 'i', 'o', 'u', 'y'];
+    let vowelsCount = 0;
+    for (let i =0; i < string.length; i++) {
+        if (vowels.includes(string[i])) {
+            vowelsCount++;
+        }
+    }
+    return vowelsCount; 
+}
+console.log(findVowelsLettesInString("string"));
