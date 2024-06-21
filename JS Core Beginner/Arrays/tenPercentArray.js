137a19ffa016b85aa7e45ec298a99f51c57190d5
Roman Biruk <105756991+romb1q@users.noreply.github.com>
2024-06-21 16:03:24 +0300

    Update tenPercentArray.js

--- JS Core Beginner/Arrays/tenPercentArray.js
+++ JS Core Beginner/Arrays/tenPercentArray.js
@@ -4,4 +4,4 @@ function tenPercentArray(array) {
     return array.map(el => el * 1.1)
 }
 
-console.log(tenPercentArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 100]));
+console.log(tenPercentArray([1, 2, 3, 4, 5]));  // [1.1, 2.2, 3.3, 4.4, 5.5]
