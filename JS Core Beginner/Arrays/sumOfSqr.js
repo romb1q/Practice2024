91086d8f185f98ae94af79fefc4f7cbb50f3cb46
Roman Biruk <105756991+romb1q@users.noreply.github.com>
2024-06-21 16:27:30 +0300

    Update sumOfSqr.js

--- JS Core Beginner/Arrays/sumOfSqr.js
+++ JS Core Beginner/Arrays/sumOfSqr.js
@@ -1,1 +1,11 @@
+// 3 task
 
+function sumOfSqr(array) {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i] * array[i];
+    }
+    return sum;
+}
+
+console.log(sumOfSqr([1,2,3,4,5,6,7]));  // 140
