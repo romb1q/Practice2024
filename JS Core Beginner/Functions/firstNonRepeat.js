ddc1d2b182e6d57f2b1341f55f4770f3bbc8cca4
Roman Biruk <105756991+romb1q@users.noreply.github.com>
2024-06-21 17:15:10 +0300

    Update firstNonRepeat.js

--- JS Core Beginner/Functions/firstNonRepeat.js
+++ JS Core Beginner/Functions/firstNonRepeat.js
@@ -1,1 +1,23 @@
+// 3 task
 
+function firstNonRepeatingChar(str) {
+    const charCount = {};
+    
+    for (let char of str) {
+        if (charCount[char]) {
+            charCount[char]++;
+        } else {
+            charCount[char] = 1;
+        }
+    }
+
+    for (let char of str) {
+        if (charCount[char] === 1) {
+            return char;
+        }
+    }
+
+    return null;
+}
+
+console.log(firstNonRepeatingChar("aaaeara")); // e
