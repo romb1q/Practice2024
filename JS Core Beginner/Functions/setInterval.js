fd5a55c50136e3e5cddda16b3f25437036b8640f
Roman Biruk <105756991+romb1q@users.noreply.github.com>
2024-06-21 16:51:50 +0300

    Update setInterval.js

--- JS Core Beginner/Functions/setInterval.js
+++ JS Core Beginner/Functions/setInterval.js
@@ -1,1 +1,20 @@
+// 1 task
 
+function callback() {
+    let randomNumber = Math.floor(Math.random() * 10);
+    console.log(randomNumber / 2);
+}
+setInterval(callback, 1000);
+/*
+
+1
+3
+2.5
+2
+0.5
+1.5
+4.5
+2.5
+3.5
+
+*/
