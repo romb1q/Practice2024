54b7b1b42d6ddbebacddf4ad51cdc1b590f55b55
Roman Biruk <105756991+romb1q@users.noreply.github.com>
2024-06-21 16:02:03 +0300

    Update halfOfArray.js

--- JS Core Beginner/Arrays/halfOfArray.js
+++ JS Core Beginner/Arrays/halfOfArray.js
@@ -1,1 +1,12 @@
+// 2 task
 
+function sumOfArraysHalf(array) {
+    let sum = 0;
+    let firstHalf = array.length / 2;
+    for (let i = 0; i < firstHalf; i++) {
+        sum += array[i];
+    }
+    return sum;
+}
+
+console.log(sumOfArraysHalf([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]));
