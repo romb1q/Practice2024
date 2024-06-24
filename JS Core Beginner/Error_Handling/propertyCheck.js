fc6d3f6845d145fee3062260c9be69d47e5fa019
Roman Biruk <105756991+romb1q@users.noreply.github.com>
2024-06-24 14:12:17 +0300

    Update propertyCheck.js

--- JS Core Beginner/Error_Handling/propertyCheck.js
+++ JS Core Beginner/Error_Handling/propertyCheck.js
@@ -1,1 +1,21 @@
+// 2 task
 
+function checkProperty(obj, prop) {
+    try {
+        let value = obj[prop];
+        return value;
+    } catch (error) {
+        if (error instanceof TypeError) {
+            return `TypeError: '${prop}' не существует`;
+        } else {
+            throw error;
+        }
+    }
+}
+
+// Примеры использования функции
+let obj1 = { name: "Паша", age: 36 };
+let obj2;
+
+console.log(checkProperty(obj1, "name")); // Паша
+console.log(checkProperty(obj2, "name")); // TypeError: 'name' не существует
