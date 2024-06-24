e0fe33c862baa3d3d836dbfbc3e4b5e74cb2c41f
Roman Biruk <105756991+romb1q@users.noreply.github.com>
2024-06-24 14:07:52 +0300

    Update isInteger.js

--- JS Core Beginner/Error_Handling/isInteger.js
+++ JS Core Beginner/Error_Handling/isInteger.js
@@ -1,1 +1,18 @@
+// 1 task
 
+const isNumberInteger = (num) => {
+    try {
+        if (!Number.isInteger(num)) {
+            // Выдача пользовательской ошибки
+            throw new Error("The provided number is not an integer.");
+        }
+        else {
+            return true;
+        }
+    } catch (error) {
+        return error.message;
+    }
+}
+
+console.log(isNumberInteger(5.9));  // The provided number is not an integer.
+console.log(isNumberInteger(5));  // true
