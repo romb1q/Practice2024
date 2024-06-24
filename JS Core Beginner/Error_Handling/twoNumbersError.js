e2c7a00731c0cfd0a1550d802d089e9e197ce35b
Roman Biruk <105756991+romb1q@users.noreply.github.com>
2024-06-24 14:18:26 +0300

    Update twoNumbersError.js

--- JS Core Beginner/Error_Handling/twoNumbersError.js
+++ JS Core Beginner/Error_Handling/twoNumbersError.js
@@ -1,1 +1,17 @@
+// 3 task
 
+function isSecondNumberNull(num1, num2) {
+    try {
+        if (num2 !== 0) {
+            return console.log(`Первое число: ${num1}, Второе число: ${num2}`);
+        }
+        else {
+            throw new Error('Второе число равно нулю');
+        }
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+isSecondNumberNull(2,1);  // Первое число: 2, Второе число: 1
+isSecondNumberNull(2,0);  // Второе число равно нулю
