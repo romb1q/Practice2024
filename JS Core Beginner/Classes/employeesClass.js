4e3e53adc05ad0903cf1b3e0fc3d411269180246
Roman Biruk <105756991+romb1q@users.noreply.github.com>
2024-06-24 12:34:52 +0300

    Update employeesClass.js

--- JS Core Beginner/Classes/employeesClass.js
+++ JS Core Beginner/Classes/employeesClass.js
@@ -1,1 +1,31 @@
+// 3 task
 
+class Employee {
+    constructor(name, salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    getYearSalary() {
+        return `Годовая зарплата работника: ${this.salary * 12}`;
+    }
+}
+
+class Manager extends Employee {
+    constructor(name, salary, department, bonus) {
+        super(name, salary);
+        this.department = department;
+        this.bonus = bonus;
+    }
+
+    getYearSalary() {
+        return `Годовая зарплата менеджера: ${(this.salary * 12) + this.bonus}`;
+    }
+}
+
+const employee1 = new Employee("Роман", 1200);
+const employee2 = new Manager("Павел", 1400, "Кадровый", 200);
+
+console.log(employee1.getYearSalary()); // Годовая зарплата работника: 14400
+
+console.log(employee2.getYearSalary()); // Годовая зарплата менеджера: 17000
