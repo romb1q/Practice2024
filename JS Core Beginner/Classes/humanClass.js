5230feedb5e87e94c2e8cd44c559a8d15b0a349b
Roman Biruk <105756991+romb1q@users.noreply.github.com>
2024-06-24 12:12:20 +0300

    Update humanClass.js

--- JS Core Beginner/Classes/humanClass.js
+++ JS Core Beginner/Classes/humanClass.js
@@ -1,1 +1,19 @@
+// 1 task
 
+class Human {
+    constructor(name, age, country) {
+        this.name = name;
+        this.age = age;
+        this.country = country;
+    }
+
+    info() {
+        return `Имя: ${this.name}, Возраст: ${this.age}, Страна проживания: ${this.country}`;
+    } 
+}
+
+const person1 = new Human("Роман", 20, "Беларусь");
+const person2 = new Human("Аркадий", 3, "США");
+const person3 = new Human("Матвей", 43, "Зимбабве");
+
+console.log(person1.info());  // Имя: Роман, Возраст: 20, Страна проживания: Беларусь
