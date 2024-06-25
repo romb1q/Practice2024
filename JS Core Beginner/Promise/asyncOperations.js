628bc25e9661933d7347b6286fc36dc9b2389eaf
Roman Biruk <105756991+romb1q@users.noreply.github.com>
2024-06-25 15:05:59 +0300

    Update asyncOperations.js

--- JS Core Beginner/Promise/asyncOperations.js
+++ JS Core Beginner/Promise/asyncOperations.js
@@ -1,1 +1,30 @@
+// 1 tsask
 
+function operation(num) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log(`Операция ${num}`);
+            resolve(num);
+        }, 1000);
+    });
+}
+
+async function runSequense(operations) {
+    for (const operation of operations) {
+        await operation();
+    }
+    console.log('Готово');
+}
+
+const operationsArray = [
+() => operation(1),
+() => operation(2),
+() => operation(3)
+];
+
+runSequense(operationsArray);
+
+// Операция 1
+// Операция 2
+// Операция 3
+// Готово
