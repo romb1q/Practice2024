42a63c9885f7e60f1f76145bde7fe7514547e478
Roman Biruk <105756991+romb1q@users.noreply.github.com>
2024-06-25 15:20:31 +0300

    Update apiPromise.js

--- JS Core Beginner/Promise/apiPromise.js
+++ JS Core Beginner/Promise/apiPromise.js
@@ -1,1 +1,29 @@
+// 3 task
+
+const urlsArray = [
+    "https://letterboxd.com/",
+    "https://www.hltv.org/",
+    "https://www.youtube.com/"
+];
+
+function fetchData(url) {
+    return fetch(url).then(response => response.text());
+}
+
+function fetchAllData(urlsArray) {
+    const promises = urlsArray.map(url => fetchData(url));
+    return Promise.all(promises).then(results => {
+        return results.reduce((acc, data) => {
+            return acc.concat(data);
+        }, []);
+    });
+}
+
+fetchAllData(urlsArray)
+    .then(combinedResult => {
+        console.log('Combined Result:', combinedResult);
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error);
+    });
 
