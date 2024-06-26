61f7256609f9931edee0b66b90c8a8722445c5a6
Roman Biruk <105756991+romb1q@users.noreply.github.com>
2024-06-26 13:38:04 +0300

    Update numberGenerator.js

--- JS Core Beginner/Event_Loop/numberGenerator.js
+++ JS Core Beginner/Event_Loop/numberGenerator.js
@@ -1,1 +1,24 @@
+// 3 task
 
+function randomTime() {
+    return new Promise((resolve, reject) => {
+        const randomNumber = Math.floor(Math.random() * 10) + 1;
+        const delay = randomNumber * 1000;
+
+        setTimeout(() => {
+            if (randomNumber <= 5) {
+                resolve(`Успешно. Число ${randomNumber}, задержка: ${delay / 1000}с.`);  // Успешно. Число 3, задержка: 3с.
+            } else {
+                reject(new Error(`Ошибка. Число ${randomNumber}, задержка: ${delay / 1000}с.`));  // Ошибка. Число 7, задержка: 7с.
+            }
+        }, delay);
+    });
+}
+
+randomTime()
+    .then(result => {
+        console.log(result);
+    })
+    .catch(error => {
+        console.error(error.message);
+    });
