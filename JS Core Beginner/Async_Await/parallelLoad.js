5575211eee5e47b0539e2d6d020c38a2eb27b704
Roman Biruk <105756991+romb1q@users.noreply.github.com>
2024-06-26 10:23:34 +0300

    Update parallelLoad.js

--- JS Core Beginner/Async_Await/parallelLoad.js
+++ JS Core Beginner/Async_Await/parallelLoad.js
@@ -1,1 +1,34 @@
+// 3 task
+
+async function fetchDataFromServer(url) {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`ошибка HTTP: ${response.status}`);
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error(`Ошибка при загрузке данных с ${url}:`, error);
+        throw error;
+    }
+}
+
+async function fetchMultipleSources() {
+    const urls = [
+        'https://api.thecatapi.com/v1/images/search', // TheCatAPI
+        'https://randomuser.me/api/'                  // RandomUser.me
+    ];
+
+    try {
+        const results = await Promise.all(urls.map(url => fetchDataFromServer(url)));  // параллельная загрузка
+        return results;
+    } catch (error) {
+        console.error('Ошибка при параллельной загрузке данных:', error);
+    }
+}
+
+fetchMultipleSources().then(results => {
+    console.log('Результаты параллельной загрузки данных:', results);
+});
 
