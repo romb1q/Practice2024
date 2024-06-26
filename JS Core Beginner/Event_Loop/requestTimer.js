6fb5cf46b3f0dd5fd5367dd19c9db0db74e0f59d
Roman Biruk <105756991+romb1q@users.noreply.github.com>
2024-06-26 12:18:56 +0300

    Update requestTimer.js

--- JS Core Beginner/Event_Loop/requestTimer.js
+++ JS Core Beginner/Event_Loop/requestTimer.js
@@ -1,1 +1,34 @@
+// 2 task
+
+function fetchWithTimeout(url, timeout, callback) {
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    const fetchPromise = fetch(url, { signal })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`ошибка HTTP: ${response.status}`);
+            }
+            return response.json();
+        });
+
+    const timeoutPromise = new Promise((_, reject) => {
+        setTimeout(() => {
+            controller.abort();
+            reject(new Error('Request timed out'));
+        }, timeout);
+    });
+
+    Promise.race([fetchPromise, timeoutPromise])
+        .then(data => callback(null, data))
+        .catch(error => callback(error, null));
+}
+
+fetchWithTimeout('https://randomuser.me/api/', 1, (error, data) => {  // Error: Request timed out, т.к. таймаут в 1 милисекунду
+    if (error) {
+        console.error('Error:', error.message);
+    } else {
+        console.log(data);
+    }
+});
 
