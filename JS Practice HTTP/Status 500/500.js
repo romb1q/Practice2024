0bf17dc6c04b4f31df28f5ac94b5ccc348f3e5b1
Roman Biruk <105756991+romb1q@users.noreply.github.com>
2024-07-02 13:49:18 +0300

    Update 500.js

--- JS Practice HTTP/Status 500/500.js
+++ JS Practice HTTP/Status 500/500.js
@@ -1,1 +1,27 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const port = 3007;
 
+app.get('/status', async (req, res) => {
+    try {
+        const response = await axios.get('https://postman-echo.com/status/500');
+        res.json({
+            api: 'Postman Echo',
+            city: 'ServerError',
+            status: response.status,
+            statusText: response.statusText
+        });
+    } catch (error) {
+        res.json({
+            api: 'Postman Echo',
+            city: 'ServerError',
+            status: error.response.status,
+            statusText: error.response.statusText
+        });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
